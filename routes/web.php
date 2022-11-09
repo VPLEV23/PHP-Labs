0c184a927acdf9b6968343a7daa47cc136c3212a
Vlad Bulgakov <48432102+VPLEV23@users.noreply.github.com>
2022-11-09 19:44:49 +0200

    lab2

--- routes/web.php
+++ routes/web.php
@@ -17,23 +17,12 @@ use App\Http\Controllers\UserController;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/users', [UserController::class, 'index'])
-    ->name('users.index');
 
-Route::get('/users/create', [UserController::class, 'create'])
-    ->name('users.create');
+Route::resource('users', UserController::class)
+->middleware('auth');
 
-Route::post('/users/', [UserController::class, 'store'])
-    ->name('users.store');
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::get('/users/{user}', [UserController::class, 'show'])
-    ->name('users.show');
-
-Route::get('/users/{user}/edit', [UserController::class, 'edit'])
-    ->name('users.edit');
-
-Route::put('/users/{user}', [UserController::class, 'update'])
-    ->name('users.update');
-
-Route::delete('/users/{user}', [UserController::class, 'destroy'])
-    ->name('users.destroy');
+require __DIR__.'/auth.php';
