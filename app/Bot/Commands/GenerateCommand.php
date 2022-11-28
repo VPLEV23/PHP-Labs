ea4786ed7c6565895398010746dda94dbe598d53
Vlad Bulgakov <48432102+VPLEV23@users.noreply.github.com>
2022-11-28 13:32:52 +0200

    lab3.1

--- app/Bot/Commands/GenerateCommand.php
+++ app/Bot/Commands/GenerateCommand.php
@@ -2,86 +2,37 @@
 
 namespace App\Bot\Commands;
 
-use Telegram\Bot\Actions;
-
+use App\Models\Chat;
+use App\Models\Chat_Participant;
 use Telegram\Bot\Commands\Command;
+use Illuminate\Support\Facades\DB;
 
 class GenerateCommand extends Command
-
 {
-
-    /**
-
-     * @var string Command Name
-
-     */
 
     protected $name = "generate";
 
-    /**
-
-     * @var string Command Description
-
-     */
-
-    protected $description = "Generate Command";
-
-    /**
-
-     * @inheritdoc
-
-     */
+    protected $description = "Generate Command to select coffee pairs";
 
     public function handle()
-
     {
+        $telegramUpdate = $this->getUpdate();
+        $telegramChat = $telegramUpdate->getChat();
 
-        // This will send a message using `sendMessage` method behind the scenes to
+        $participantsNumber = DB::table('chat_participants')
+                                    ->where('chat_id', '=', $telegramChat->id)
+                                    ->count();
 
-        // the user/chat id who triggered this command.
+        if($participantsNumber >= 2)
+            try {
+                Chat_Participant::getRandomPairs($telegramChat->id);
 
-        // `replyWith<Message|Photo|Audio|Video|Voice|Document|Sticker|Location|ChatAction>()` all the available methods are dynamically
-
-        // handled when you replace `send<Method>` with `replyWith` and use the same parameters - except chat_id does NOT need to be included in the array.
-
-        $this->replyWithMessage(['text' => 'Hello! Welcome to our bot, Here are our available commands:']);
-
-        // This will update the chat status to typing...
-
-        $this->replyWithChatAction(['action' => Actions::TYPING]);
-
-        // This will prepare a list of available commands and send the user.
-
-        // First, Get an array of all registered commands
-
-        // They'll be in 'command-name' => 'Command Handler Class' format.
-
-        $commands = $this->getTelegram()->getCommands();
-
-        // Build the list
-
-        $response = '';
-
-        foreach ($commands as $name => $command) {
-
-            $response .= sprintf('/%s - %s' . PHP_EOL, $name, $command->getDescription());
-
-        }
-
-        // Reply with the commands list
-
-        $this->replyWithMessage(['text' => $response]);
-
-        // Trigger another command dynamically from within this command
-
-        // When you want to chain multiple commands within one or process the request further.
-
-        // The method supports second parameter arguments which you can optionally pass, By default
-
-        // it'll pass the same arguments that are received for this command originally.
-
-        $this->triggerCommand('subscribe');
+                $this->replyWithMessage(['text' => 'Done']);
+            } catch (\Exception $exception) {
+                $this->replyWithMessage(['text' => "Oops... Something went wrong. {$exception->getMessage()}"]);
+            }
+        else
+            $this->replyWithMessage(['text' => "Number of participants is less than two"]);
 
     }
-
 }
--- app/Models/Chat.php
+++ app/Models/Chat.php
@@ -2,18 +2,28 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use \Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Support\Facades\DB;
 
 class Chat extends Model
-
 {
-
-    public function chatParticipant()
-
+    public function chatParticipants()
     {
+        return $this->hasMany(Chat_Participant::class);
+    }
 
-        return $this->hasMany(ChatParticipant::class);
+    public static function registerChat($chat_id)
+    {
+        DB::table('chats')->insert([
+            'chat_id' => $chat_id
+        ]);
+    }
 
+    public static function unregisterChat($chat_id)
+    {
+        DB::table('chats')->where('chat_id', '=', $chat_id)->delete();
     }
 
 }
